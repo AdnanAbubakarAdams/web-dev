0a0ebe028132021a65f5e6355167f5b3c0625bd8
AdnanAbubakarAdams <adnan_abubakaradams@yahoo.com>
2023-01-02 20:09:40 -0500

    back-end routes setting up

--- app.js
+++ app.js
@@ -4,6 +4,12 @@ const express = require('express');
 // initialize app
 const app = express();
 
+// controllers
+const studentsController = require('./controllers/studentsController');
+
+// middlewares
+app.use('/students', studentsController);
+
 // route
 app.get("/", (req, res) => {
     res.send("Come on Adnan lets go")
---
+++ controllers/studentsController.js
@@ -0,0 +1,38 @@
+const express = require("express");
+const { response } = require("../app");
+
+const controller = express.Router();
+
+const studentsData = require("../studentData.json");
+
+// routes
+controller.get("/", (req, res) => {
+  res.json(studentsData);
+});
+
+// single student
+controller.get("/:id", (req, res) => {
+  try {
+    const studentId = req.params.id;
+
+    if (!/[0-9]/.test(studentId)) {
+      res.send("student id must tbe a number");
+      return;
+    }
+
+    const singleStudent = studentsData.students.find((student) => {
+      return student.id === studentId;
+    });
+    console.log(singleStudent);
+    if (singleStudent) {
+      res.json(singleStudent);
+    } else {
+      res.send("Student Not found");
+    }
+  } catch (error) {
+    res.status(500).send("An error occured")
+  }
+
+});
+
+module.exports = controller;

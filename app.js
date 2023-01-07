832e67839f7fb195f9d6c1ef4b0e4f0d327190b6
AdnanAbubakarAdams <adnan_abubakaradams@yahoo.com>
2023-01-06 23:45:06 -0500

    added some more routes with functions

--- app.js
+++ app.js
@@ -4,6 +4,9 @@ const express = require('express');
 // initialize app
 const app = express();
 
+// utils
+const {orderedWordCounter, consecutiveVowels} = require(`./utils/Functions`)
+
 // controllers
 const studentsController = require('./controllers/studentsController');
 const namesController = require('./controllers/namesController')
@@ -13,8 +16,40 @@ app.use('/students', studentsController);
 app.use('/names', namesController);
 
 // route
+// app.get("/", (req, res) => {
+//     res.send("Come on Adnan lets go")
+// })
+
 app.get("/", (req, res) => {
-    res.send("Come on Adnan lets go")
+    res.send(
+        {greeting: "Hello Word!", 
+        source: ["Mason", "Derick", "Adnan"]}
+        )
+})
+
+app.get("/add", (req, res) => {
+    res.send(
+        "add your numbers in the URL"
+    )
+})
+
+app.get("/add/:num1/:num2", (req,res) =>{
+    
+    const {num1, num2} = req.params;
+    let total = Number(num1) + Number(num2)
+
+    //responses sent must be in a string format
+    res.send(`${total}`)
+})
+
+app.get("/consecutiveVowels/:word", (req, res) => {
+    let {word} = req.params;
+    res.send(String(consecutiveVowels(word)))
+})
+
+app.get("/ordedCount/:text", (req, res) => {
+  let {text} = req.params;
+  res.send(orderedWordCounter(text));
 })
 
 
